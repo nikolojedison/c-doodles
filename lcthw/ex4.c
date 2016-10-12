53ddfff857af20ace2ab67b91743aa752df6c300
Nik Mallett <nikolojedison@gmail.com>
2016-10-12 10:19:29 -0500

    Exercises 4 and 5

---
+++ lcthw/ex4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* Program doesn't work on purpose. */
+
+int main()
+{
+	int age = 10;
+	int height;
+
+	printf("I am %d years old. \n");
+	printf("I am %d inche tall. \n", height);
+
+	return 0;
+}
+
---
+++ lcthw/ex5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* This is a multiline comment. */
+int main(int argc, char *argv[])
+{
+	int distance = 100;
+
+	// This is a single line comment
+	printf("You are %d miles away.\n", distance);
+
+	return 0;
+}
